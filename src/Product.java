e19de65506141922fdea904406951a52f40e2a1a
Julien <jzapata@live.nl>
2020-04-02 16:59:22 +0200

    Added class Product

---
+++ src/Product.java
@@ -0,0 +1,20 @@
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
