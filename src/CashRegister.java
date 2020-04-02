b92a4f95f2f8866869c1a4ea093e31657cf05732
Julien <jzapata@live.nl>
2020-04-02 15:58:50 +0200

    Initial commit

---
+++ src/CashRegister.java
@@ -0,0 +1,20 @@
+public class CashRegister {
+    private double total = 0.0;
+    public int count = 0;
+
+    public CashRegister() {
+
+    }
+
+    public void addItem(double price) {
+
+    }
+
+    public double computeTotal() {
+        return 0.0;
+    }
+
+    public void clear() {
+
+    }
+}
