ca634f6cb7627b74bb58ed33bc826705acba11a5
Julien <jzapata@live.nl>
2020-04-02 16:08:27 +0200

    Added comments to properties and methods

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,19 +1,23 @@
 public class CashRegister {
+    /* Keep track of the total price */
     private double total = 0.0;
+    /* Keep track of the item count */
     public int count = 0;
 
     public CashRegister() {
 
     }
-
+    /* Adds a product with price */
     public void addItem(double price) {
 
     }
 
+    /* Calculates the price of an order (including discount) */
     public double computeTotal() {
         return 0.0;
     }
 
+    /* Clears the register for the next customer */
     public void clear() {
 
     }
