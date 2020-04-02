321d2965bf443768f0e5c9f70ff1411cc68f30ce
Julien <jzapata@live.nl>
2020-04-02 16:24:23 +0200

    Created tests for computeTotal, getCount and clear

--- test/CashRegisterTest.java
+++ test/CashRegisterTest.java
@@ -9,10 +9,10 @@ TODO: Als ik daarna clear () aanroep moet computeTotal () € 0 opleveren en moe
 */
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CashRegisterTest {
-
 
     @Test
     public void addItem() {
@@ -20,6 +20,24 @@ public class CashRegisterTest {
 
     @Test
     public void computeTotal() {
+        CashRegister c = new CashRegister();
+        Assert.assertEquals(0.0, c.computeTotal(), 0.00001);
+        Assert.assertEquals(0, c.getCount());
+        c.addItem(10);
+        Assert.assertEquals(1, c.getCount());
+        Assert.assertEquals(10.0, c.computeTotal(), 0.00001);
+        c.addItem(20);
+        Assert.assertEquals(2, c.getCount());
+        Assert.assertEquals(30.0, c.computeTotal(), 0.00001);
+        c.addItem(20);
+        Assert.assertEquals(50.0, c.computeTotal(), 0.00001);
+        Assert.assertEquals(3, c.getCount());
+        c.addItem(60);
+        Assert.assertEquals(99.0, c.computeTotal(), 0.00001);
+        Assert.assertEquals(4, c.getCount());
+        c.clear();
+        Assert.assertEquals(0.0, c.computeTotal(), 0.00001);
+        Assert.assertEquals(0, c.getCount());
     }
 
     @Test
