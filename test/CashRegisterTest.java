26de83ebcbf29a8bb26ada8b70aa6b743897604e
Julien <jzapata@live.nl>
2020-04-02 16:31:43 +0200

    Removed finished TODOs

--- test/CashRegisterTest.java
+++ test/CashRegisterTest.java
@@ -1,14 +1,3 @@
-/*
-TODO: Als ik het programma start, moet computeTotal () € 0 opleveren en moet getCount () 0 als resultaat teruggeven.
-TODO: Als ik een item toevoeg met een prijs van € 10, moet computeTotal () € 10 opleveren en moet getCount 1 als resultaat teruggeven.
-TODO: Als ik nog een item toevoeg met een prijs van € 20, moet computeTotal () € 30 opleveren en moet getCount 2 als resultaat teruggeven.
-TODO: Als ik nog een item toevoeg met een prijs van € 20, moet computeTotal () € 50 opleveren en moet getCount 3 als resultaat teruggeven (LET OP: zonder korting, want totaalprijs is nog lager dan €
-    - 100,00).
-TODO: Als ik nog een item toevoeg met een prijs van € 60, moet computeTotal () € 99 opleveren en moet getCount 4 als resultaat teruggeven (LET OP: nu wel met korting!!!).
-TODO: Als ik daarna clear () aanroep moet computeTotal () € 0 opleveren en moet getCount 0 als resultaat teruggeven.
-*/
-
-
 import org.junit.Assert;
 import org.junit.Test;
 
